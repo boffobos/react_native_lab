f82608f71edf5c89bef11bb431abdf6792687da8
Denis Zhurov <den_fobos@mail.ru>
2022-03-19 17:05:35 +0300

    added animated splash screen

--- App.tsx
+++ App.tsx
@@ -10,8 +10,7 @@
 
 import React from 'react';
 import {
-	Alert,
-  SafeAreaView,
+	Animated,
   ScrollView,
   StatusBar,
   StyleSheet,
@@ -19,6 +18,7 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import MaskedView from '@react-native-community/masked-view'
 import SplashScreen from 'react-native-splash-screen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import {Button, ThemeProvider } from 'react-native-elements';
@@ -34,38 +34,53 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import AnimatedSplash from 'react-native-animated-splash-screen';
+
 import { Provider } from 'react-redux';
 import { store, persistor } from './src/Redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { generalTheme } from './src/Theme';
-import { BottomTabs } from './src/exports';
-import { FONT_REGULAR } from './src/config';
+import { FONT_REGULAR, PRIMARY_COLOR } from './src/config';
+
+
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
+	const [loaded, setLoaded] = React.useState(false)
 	React.useEffect(() => {
 		SplashScreen.hide();
 	},[]);
+
+	React.useEffect(() => {
+		setTimeout(() => setLoaded(true), 500) //Use this async function to
+	}, [])
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
   return (
-		<Provider store={store}>
-			<PersistGate loading={null} persistor={persistor}>
-				<NavigationContainer>
-					<SafeAreaProvider>
-						<ThemeProvider theme={generalTheme}>
-						{/* <GlobalTheme> */}
-							<StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-							<MyNavigation />
-						{/* </GlobalTheme> */}
-						</ThemeProvider>
-					</SafeAreaProvider>
-				</NavigationContainer>
-			</PersistGate>
-		</Provider>
+		<AnimatedSplash
+			logoImage={require('./src/Assets/Images/logo_splash_screen.png')}
+			isLoaded={loaded}
+			backgroundColor={PRIMARY_COLOR}
+			translucent={true}
+		>
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<NavigationContainer>
+						<SafeAreaProvider>
+							<ThemeProvider theme={generalTheme}>
+							{/* <GlobalTheme> */}
+								<StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+								<MyNavigation />
+							{/* </GlobalTheme> */}
+							</ThemeProvider>
+						</SafeAreaProvider>
+					</NavigationContainer>
+				</PersistGate>
+			</Provider>
+		</AnimatedSplash>
   );
 };
 
